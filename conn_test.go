462366ae21e8275a817886cad86c7f8f6d01491c
Bryan Moyles <bryan@teltech.co>
2016-05-26 03:16:44 -0400

    Just trying to ensure code coverage by checking every proxied func

--- conn_test.go
+++ conn_test.go
@@ -10,22 +10,29 @@ import (
 )
 
 type mockConn struct {
-	closedCounter int
+	closedCounter   int
+	functionsCalled []string
+}
+
+func (c *mockConn) appendCall(functionName string) {
+	c.functionsCalled = append(c.functionsCalled, functionName)
 }
 
 func (c *mockConn) Close() error {
+	c.appendCall("Close")
+
 	c.closedCounter += 1
 
 	return nil
 }
 
-func (c *mockConn) LocalAddr() net.Addr                { return nil }
-func (c *mockConn) Read(b []byte) (int, error)         { return 0, nil }
-func (c *mockConn) RemoteAddr() net.Addr               { return nil }
-func (c *mockConn) SetDeadline(t time.Time) error      { return nil }
-func (c *mockConn) SetReadDeadline(t time.Time) error  { return nil }
-func (c *mockConn) SetWriteDeadline(t time.Time) error { return nil }
-func (c *mockConn) Write(b []byte) (int, error)        { return 0, nil }
+func (c *mockConn) LocalAddr() net.Addr                { c.appendCall("LocalAddr"); return nil }
+func (c *mockConn) Read(b []byte) (int, error)         { c.appendCall("Read"); return 0, nil }
+func (c *mockConn) RemoteAddr() net.Addr               { c.appendCall("RemoteAddr"); return nil }
+func (c *mockConn) SetDeadline(t time.Time) error      { c.appendCall("SetDeadline"); return nil }
+func (c *mockConn) SetReadDeadline(t time.Time) error  { c.appendCall("SetReadDeadline"); return nil }
+func (c *mockConn) SetWriteDeadline(t time.Time) error { c.appendCall("SetWriteDeadline"); return nil }
+func (c *mockConn) Write(b []byte) (int, error)        { c.appendCall("Write"); return 0, nil }
 
 func writeToChanIfWaitGroupDone(wg *sync.WaitGroup) chan bool {
 	wgDone := make(chan bool)
@@ -146,5 +153,41 @@ func TestConnClose(t *testing.T) {
 		So(c.Close(), ShouldResemble, errConnClosed)
 		So(mockConn.closedCounter, ShouldEqual, 1)
 		So(c.closed, ShouldBeTrue)
+	})
+}
+
+func TestConnParentProxy(t *testing.T) {
+	Convey("Ensure that the parent is called for every proxied func", t, func() {
+		mockConn := &mockConn{}
+
+		c := &conn{parentConn: mockConn}
+
+		So(mockConn.functionsCalled, ShouldNotResemble, []string{
+			"LocalAddr",
+			"Read",
+			"RemoteAddr",
+			"SetDeadline",
+			"SetReadDeadline",
+			"SetWriteDeadline",
+			"Write",
+		})
+
+		c.LocalAddr()
+		c.Read([]byte{})
+		c.RemoteAddr()
+		c.SetDeadline(time.Now())
+		c.SetReadDeadline(time.Now())
+		c.SetWriteDeadline(time.Now())
+		c.Write([]byte{})
+
+		So(mockConn.functionsCalled, ShouldResemble, []string{
+			"LocalAddr",
+			"Read",
+			"RemoteAddr",
+			"SetDeadline",
+			"SetReadDeadline",
+			"SetWriteDeadline",
+			"Write",
+		})
 	})
 }
